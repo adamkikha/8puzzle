2ece725973c5557f6fca9a88013d3599600d76b8
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-24 18:44:18 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -4,9 +4,6 @@ import pygame
 
 class Button:
 
-    FONT_SIZE = 12
-    
-
     def __init__(self, x_pos, y_pos, width, height, color, label, text_color,font_size):
         self.x_pos = x_pos
         self.y_pos = y_pos
@@ -26,10 +23,7 @@ class Button:
         pygame.draw.rect(screen, self.color, self.rect)
         # screen.blit(self.text, (self.x_pos+int(self.width/3), self.y_pos+int(self.height/4)))
         screen.blit(self.text, (self.x_pos, self.y_pos))
-    
-    def update(self,screen):
-        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
-        self.draw(screen)
+
 
     def check_clicked(self, x_clicked , y_clicked):
         if( x_clicked >= self.x_pos 
